5dcc0b7427242992d04c196e42f846cd59795e27
Oğuz Kağan ER <oguzkaganerr@gmail.com>
2022-10-16 23:36:09 +0300

    16.10.2022 - 23:35 - Ders Sonu

---
+++ AbstractClasses/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace AbstractClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MevzuatBase aMevzuat = new AMevzuat();
+            aMevzuat.Degerlendir();
+
+
+            Console.ReadLine();
+        }
+    }
+
+    abstract class MevzuatBase
+    {
+        public abstract void Degerlendir();
+        public void Kaydet()
+        {
+            Console.WriteLine("Kaydedildi.");
+        }
+
+        protected MevzuatBase()
+        {
+            Console.WriteLine("Çalışıyor.");
+        }
+    }
+
+
+
+    class AMevzuat : MevzuatBase
+    {
+        public override void Degerlendir()
+        {
+            Console.WriteLine("A Mevzuatı...");
+        }
+
+    }
+
+    class BMevzuat : MevzuatBase
+    {
+        public override void Degerlendir()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Interfaces3/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace Interfaces3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IPersonManager personManager = new CustomerManager();
+            personManager.Update();
+
+            IPersonManager personManager1 = new EmployeeManager();
+            personManager1.Add();
+
+            ProjectManager projectManager = new ProjectManager();
+            projectManager.Add(new CustomerManager());
+
+            projectManager.Add(new InternManager());
+            
+            
+            
+            Console.ReadLine();
+        }
+    }
+
+    interface IPersonManager
+    {
+        void Add();
+        void Update();
+    }
+
+    class CustomerManager : IPersonManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Müşteri eklendi.");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Müşteri güncellendi.");
+        }
+    }
+
+    class EmployeeManager : IPersonManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Personel eklendi.");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Personel güncellendi.");
+        }
+    }
+
+    class InternManager : IPersonManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Stajyer eklendi.");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Stajyer güncellendi.");
+        }
+    }
+
+    class ProjectManager
+    {
+        public void Add(IPersonManager personManager)
+        {
+            personManager.Add();
+        }
+    }
+
+}
